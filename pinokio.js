3f0c7e46b15149c3cbba2f4dff95c14d1b4ae84f
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-05 12:02:24 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "1.5",
   title: "PCM",
-  description: "Phased Consistency Model - generate high quality images with 2 steps https://g-u-n.github.io/projects/pcm/",
+  description: "Phased Consistency Model - generate high quality images with 2 steps https://huggingface.co/spaces/radames/Phased-Consistency-Model-PCM",
   icon: "icon.png",
   menu: async (kernel) => {
     let installing = await kernel.running(__dirname, "install.js")
